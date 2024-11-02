2ab362b02276afed6bb904bd64fc31fb4e7b42ae
Manuel Zago <zagomanuel@yahoo.it>
2024-11-02 21:50:44 +0100

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import './App.css';
+
+function App() {
+  const [info, setInfo] = useState([])
+  useEffect(() => {
+    fetch('http://localhost:4000/')
+      .then(res => res.json())
+      .then(data => setInfo(data))
+  }, [])
+
+  return (
+    <div className="App">
+    <header className="App-header">
+      <h1>Cloud, Microservizi e Scalabilità</h1>
+      <h2>Classe 5DS</h2>
+      <div className="features-container">
+        {info && info.map(item => (
+          <div key={item.id} className="feature-card">
+            <h3>{item.titolo}</h3>
+            <p>{item.caratteristica}</p>
+          </div>
+        ))}
+      </div>
+    </header>
+</div>
+
+  );
+}
+
+export default App;
